35a7d468c174e311d9a2a393fa91f58f12397342
brandiw <brandimichelle.0911@gmail.com>
2018-03-14 11:56:03 -0700

    Top-level items highlight depending on due date and can be marked as done

--- ClientApp/components/ToDoList.tsx
+++ ClientApp/components/ToDoList.tsx
@@ -1,25 +1,42 @@
 import * as React from 'react';
 import { RouteComponentProps } from 'react-router';
 
+let counter = 1;
+
 class Item {
-    main: string;
-    subs: string[];
+    id: number;
+    description: string;
+    parent: Item;
+    children: Item[];
     isDone: boolean;
+    dueDate: Date;
+
+    constructor(desc: string, due: Date){
+        this.id = counter;
+        counter += 1;
+        this.description = desc;
+        this.dueDate = due;
+        this.parent = this;
+        this.children = [];
+        this.isDone = false;
+    }
 }
 
 interface ToDoState {
-    items: string[];
+    items: Item[];
     message: string;
     newItem: string;
+    newDueDate: string;
 }
 
-class ToDoList extends React.Component<RouteComponentProps<{}>, ToDoState> {
+export class ToDoList extends React.Component<RouteComponentProps<{}>, ToDoState> {
     constructor() {
         super();
         this.state = {
             items: [],
             message: '',
-            newItem: ''
+            newItem: '',
+            newDueDate: ''
         };
     }
 
@@ -28,13 +45,57 @@ class ToDoList extends React.Component<RouteComponentProps<{}>, ToDoState> {
     }
 
     addItem = () => {
-        this.setState({
-            items: ['TBD: The thing']
-        });
+        if(this.state.newItem == '' || this.state.newDueDate == ''){
+            this.setState({
+                message: 'Please enter a description and a due date'
+            });
+        }
+        else {
+            // Correct timezone bug
+            let due = new Date(this.state.newDueDate);
+            due.setMinutes(due.getMinutes() + due.getTimezoneOffset());
+
+            let newItemObj = new Item(this.state.newItem, due);
+            let items = this.state.items;
+            items.push(newItemObj);
+
+            this.setState({
+                items: items,
+                newItem: '',
+                message: ''
+            });
+        }
+    }
+
+    markDone = (item: Item) => {
+        console.log('mark done top');
+        let updatedItems = this.state.items;
+
+        for(var i = 0; i < updatedItems.length; i++){
+            if(updatedItems[i].id == item.id){
+                updatedItems[i].isDone = true;
+                for(var j = 0; j < updatedItems[i].children.length; j++){
+                    updatedItems[i].children[j].isDone = true;
+                    for(var k = 0; k < updatedItems[i].children[j].children.length; k++){
+                        updatedItems[i].children[j].children[k].isDone = true;
+                    }
+                }
+            }
+        }
+        this.setState({ items: updatedItems });
     }
 
     addSubTask = () => {
         console.log('TO DO');
+    }
+
+    checkAllChildrenDone = () => {
+        console.log('checking children')
+    }
+
+    dueDateChange = (e: any) => {
+        console.log(e.target.value);
+        this.setState({ newDueDate: e.target.value, message: '' });
     }
 
     newItemChange = (e: any) => {
@@ -48,14 +109,15 @@ class ToDoList extends React.Component<RouteComponentProps<{}>, ToDoState> {
     }
 
     public render() {
+        console.log('rendering', this.state)
         {/* Build existing list of to-do items */}
         const itemsList = this.state.items.map(item => {
-            return <ToDoListItem li={item} />;
+            return <ToDoListItem key={item.description} item={item} markDone={this.markDone} />;
         });
 
         return <div>
             <div className="row">
-                <div className="col-xs-6">
+                <div className="col-xs-10 col-xs-offset-1">
                     <h1>To-Do List</h1>
                     <h2>What do you need to do today?</h2>
 
@@ -66,35 +128,94 @@ class ToDoList extends React.Component<RouteComponentProps<{}>, ToDoState> {
                             <label>Task Description</label>
                             <input type="text" className="form-control" placeholder="What are you doing?" onChange={this.newItemChange} value={this.state.newItem}  />
                         </div>
-                    </form>
-                    <form>
                         <div className="form-group">
-                            <label>Sub Task:</label>
-                            <input type="text" className="form-control" placeholder="A step in the process" onChange={this.subTaskChange}  />
+                            <label>Due Date:</label>
+                            <input type="date" className="form-control" defaultValue={new Date().toDateString()} onChange={this.dueDateChange} value={this.state.newDueDate} />
                         </div>
-                        <input type="submit" value="Submit Sub-Task" className="btn btn-primary" onClick={this.addSubTask} />
                     </form>
 
                     {/* Buttons to add to or clear the list */}
                     <button onClick={ () => { this.addItem() } }>Submit Item</button>
                     <button onClick={ () => { this.clear() } }>Clear</button>
                 </div>
-                <div className="col-xs-6">
-                    <div>Preview Container</div>
-                </div>
             </div>
             <hr />
-            <h2>My List</h2>
-            { itemsList }
+            <div className="row">
+                <div className="col-xs-10 col-xs-offset-1">
+                    <h2>My List</h2>
+                    { itemsList }
+                </div>
+            </div>
         </div>;
     }
 }
 
-class ToDoListItem extends React.Component {
-    public render(){
-        const classes = 'well ' + (true ? 'strike' : '');
-        return <div className={classes}>{this.props.item}</div>;
-    }
+interface ListItemProps {
+    item: Item;
+    markDone: Function;
 }
 
-export default ToDoList;
+class ToDoListItem extends React.Component<ListItemProps> {
+    addDays = (date: Date, numberOfDays: number) => {
+        date.setDate(date.getDate() + numberOfDays);
+        return date;
+    }
+
+    addSubItem = () => {
+        console.log('show form');
+    }
+
+    markDone = () => {
+        this.props.markDone(this.props.item);
+    }
+
+    deleteItem = () => {
+        console.log('del item');
+    }
+
+    public render(){
+        {/* If it is marked done, add a strikethrough */}
+        let classes = 'alert';
+
+        {/* Highlight depending on the due date (past, soon, or other) */}
+        let now = new Date();
+        now.setHours(0, 0, 0, 0);
+        let note = '';
+        console.log(now, 'comparing to', this.props.item.dueDate)
+        if(this.props.item.dueDate < now){
+            classes += ' alert-info';
+            note = '(Past Due)'
+        }
+        else if(this.props.item.dueDate <= this.addDays(now, 1)){
+            classes += ' alert-warning';
+            note = '(DUE SOON!)';
+        }
+        else {
+            classes += ' alert-success';
+        }
+
+        return <div className={classes}>
+            <div className="row">
+                <div className="col-xs-8">
+                    <span className={this.props.item.isDone ? 'strike' : ''}>
+                        {note} {this.props.item.description}
+                        <h6>Due: {this.props.item.dueDate.toDateString()}</h6>
+                    </span>
+                </div>
+                <div className="col-xs-4 text-right">
+                    <span className={this.props.item.isDone ? 'hidden' : ''}>
+                        <span onClick={this.addSubItem}>
+                            <span className="glyphicon glyphicon-plus text-primary" aria-hidden="true"></span>
+                        </span>
+                        <span onClick={this.markDone}>
+                            <span className="glyphicon glyphicon-ok text-success" aria-hidden="true"></span>
+                        </span>
+                    </span>
+                    <span onClick={this.deleteItem}>
+                        <span className="glyphicon glyphicon-remove text-danger" aria-hidden="true"></span>
+                    </span>
+                </div>
+            </div>
+        </div>;
+    }
+}
